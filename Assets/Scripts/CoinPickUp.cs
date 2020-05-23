37efacb08fa787f7fe6f533ea1e6d4896334d6aa
msuddin <mo@thetestroom.com>
2020-05-23 16:07:48 +0100

    Coin pickup SFX

--- Assets/Scripts/CoinPickUp.cs
+++ Assets/Scripts/CoinPickUp.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickUp : MonoBehaviour
+public class CoinPickUp : MonoBehaviour
 {
+    [SerializeField] AudioClip coinPickUpSfx;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioSource.PlayClipAtPoint(coinPickUpSfx, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
