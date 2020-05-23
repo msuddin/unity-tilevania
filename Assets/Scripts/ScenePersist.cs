8d78eded88aa27231398ce86861d02cf9991c3fe
msuddin <mo@thetestroom.com>
2020-05-23 16:38:21 +0100

    Scene persister

---
+++ Assets/Scripts/ScenePersist.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePersist : MonoBehaviour
+{
+
+    int startingSceneIndex;
+
+    private void Awake()
+    {
+        int currentNumberOfInstances = FindObjectsOfType<GameSession>().Length;
+
+        if (currentNumberOfInstances > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    
+    void Start()
+    {
+        startingSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    void Update()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex != startingSceneIndex)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
